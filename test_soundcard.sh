bee476a954b479e8b03786863db4ad32e2c1a8ca
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2026-01-08 09:04:30 +0000

    update

--- test_soundcard.sh
+++ test_soundcard.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#### USB SOUND CARD ####
-source "${BASH_SOURCE%/*}/functions/sound_card.sh"
-soundcard
-#### NODE Selection ####
-source "${BASH_SOURCE%/*}/functions/node_type.sh"
-nodeoption
-echo -e "$(date)" "${YELLOW} #### Node Type: $NODEOPTION #### ${NORMAL}" | sudo tee -a /var/log/install.log
-
-echo -e "$(date)" "${YELLOW} #### Sound Card: $HID $GPIOD $card #### ${NORMAL}" | sudo tee -a /var/log/install.log 
-echo -e "$(date)" "${YELLOW} #### Checking Alsa #### ${NORMAL}" | sudo tee -a /var/log/install.log 
-
+##### USB SOUND CARD ####
+#source "${BASH_SOURCE%/*}/functions/sound_card.sh"
+#soundcard
+##### NODE Selection ####
+#source "${BASH_SOURCE%/*}/functions/node_type.sh"
+#nodeoption
+#echo -e "$(date)" "${YELLOW} #### Node Type: $NODEOPTION #### ${NORMAL}" | sudo tee -a /var/log/install.log
+#
+#echo -e "$(date)" "${YELLOW} #### Sound Card: $HID $GPIOD $card #### ${NORMAL}" | sudo tee -a /var/log/install.log 
+#echo -e "$(date)" "${YELLOW} #### Checking Alsa #### ${NORMAL}" | sudo tee -a /var/log/install.log 
+sound_cards=$(cat /proc/asound/cards)
+echo $sound_cards
