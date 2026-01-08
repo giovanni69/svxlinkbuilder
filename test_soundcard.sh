53dd8f8f78896b198e004398dc089ae04b8302c2
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2026-01-08 09:02:44 +0000

    test mode

---
+++ test_soundcard.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#### USB SOUND CARD ####
+source "${BASH_SOURCE%/*}/functions/sound_card.sh"
+soundcard
+#### NODE Selection ####
+source "${BASH_SOURCE%/*}/functions/node_type.sh"
+nodeoption
+echo -e "$(date)" "${YELLOW} #### Node Type: $NODEOPTION #### ${NORMAL}" | sudo tee -a /var/log/install.log
+
+echo -e "$(date)" "${YELLOW} #### Sound Card: $HID $GPIOD $card #### ${NORMAL}" | sudo tee -a /var/log/install.log 
+echo -e "$(date)" "${YELLOW} #### Checking Alsa #### ${NORMAL}" | sudo tee -a /var/log/install.log 
+
