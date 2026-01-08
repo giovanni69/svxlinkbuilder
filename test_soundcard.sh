0d05603d1b58257ca295c80122e448e9aa412326
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2026-01-08 09:08:01 +0000

    update

--- test_soundcard.sh
+++ test_soundcard.sh
@@ -10,4 +10,10 @@
 #echo -e "$(date)" "${YELLOW} #### Sound Card: $HID $GPIOD $card #### ${NORMAL}" | sudo tee -a /var/log/install.log 
 #echo -e "$(date)" "${YELLOW} #### Checking Alsa #### ${NORMAL}" | sudo tee -a /var/log/install.log 
 sound_cards=$(cat /proc/asound/cards)
-echo $sound_cards
+echo $sound_cards | grep -q 'wm8960soundcard'
+if [ $? -eq 0 ]; then
+    echo "WM8960 sound card detected:" 
+else
+    echo "WM8960 sound card not detected."
+fi
+
